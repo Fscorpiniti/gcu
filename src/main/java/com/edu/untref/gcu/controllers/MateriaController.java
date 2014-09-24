003990df431b61b1d4fd0867f51777dbac9380c5
Guillermo Arispe <guillermo.arispe@gmail.com>
2014-09-24 20:40:20 -0300

    Se agregaron ejemplos de etodos POST y GET

--- src/main/java/com/edu/untref/gcu/controllers/MateriaController.java
+++ src/main/java/com/edu/untref/gcu/controllers/MateriaController.java
@@ -5,24 +5,39 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.edu.untref.gcu.domain.Materia;
 import com.edu.untref.gcu.services.MateriaService;
 
 @Controller
-@RequestMapping("/materias")
 public class MateriaController {
 
 	@Resource(name = "materiaService")
 	private MateriaService materiaService;
 	
 	@ResponseBody
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/materias",method = RequestMethod.GET)
 	public List<Materia> getAllMaterias() {
 		return materiaService.getAllMaterias();
 	}
 
+	@ResponseBody
+	@RequestMapping(value = "/materias/{id}", method = RequestMethod.GET)
+	public Materia getMateria(@PathVariable String id, @RequestParam String order) {
+		order.toString();
+		return this.materiaService.findById(id);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/materias", method = RequestMethod.POST)
+	public Materia createMateria(@RequestBody Materia materia) {
+		return this.materiaService.save(materia);
+	}
+	
 }
--- src/main/java/com/edu/untref/gcu/services/MateriaService.java
+++ src/main/java/com/edu/untref/gcu/services/MateriaService.java
@@ -8,4 +8,8 @@ public interface MateriaService {
 
 	List<Materia> getAllMaterias();
 	
+	Materia findById(String id);
+	
+	Materia save(Materia materia);
+	
 }
