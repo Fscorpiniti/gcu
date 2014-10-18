8254d9a4ddab322cc2962fc96eff2a6f36ef16de
Guillermo Arispe <guillermo.arispe@gmail.com>
2014-10-18 17:25:29 -0300

    Quito metodo sin sentido

--- src/main/java/com/edu/untref/gcu/daos/PlanMateriaDAO.java
+++ src/main/java/com/edu/untref/gcu/daos/PlanMateriaDAO.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import com.edu.untref.gcu.domain.PlanMateria;
 
-public interface PlanMateriaDAO extends GenericDAO<PlanMateria, Serializable>{
+public interface PlanMateriaDAO extends GenericDAO<PlanMateria, Serializable> {
 
 	List<PlanMateria> getAllPlanMateriasByIdPlanEstudio(String id);
-	List<PlanMateria> getAllPlanMateriaByMateria(String id);
-	}
+}
