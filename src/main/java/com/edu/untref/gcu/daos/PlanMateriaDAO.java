707f04acec051ff27d2887bccfa53d459898f33e
Guillermo Arispe <guillermo.arispe@gmail.com>
2014-10-18 14:09:07 -0300

    Se agrega metodo faltante en interface

--- src/main/java/com/edu/untref/gcu/daos/PlanMateriaDAO.java
+++ src/main/java/com/edu/untref/gcu/daos/PlanMateriaDAO.java
@@ -8,5 +8,5 @@ import com.edu.untref.gcu.domain.PlanMateria;
 public interface PlanMateriaDAO extends GenericDAO<PlanMateria, Serializable>{
 
 	List<PlanMateria> getAllPlanMateriasByIdPlanEstudio(String id);
-
-}
+	List<PlanMateria> getAllPlanMateriaByMateria(String id);
+	}
