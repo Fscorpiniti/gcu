6755abc8550f8b673e886c9e25841658c3f36edb
fernando <fernando.scorpiniti@sayqa.com>
2014-10-22 00:12:57 -0300

    Se cambio de paquete la suite

--- src/test/java/com/edu/untref/gcu/test/suites/TestSuiteGcu.java
+++ src/test/java/com/edu/untref/gcu/test/suites/TestSuiteGcu.java
@@ -1,8 +1,11 @@
-package com.edu.untref.gcu.test.services;
+package com.edu.untref.gcu.test.suites;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import com.edu.untref.gcu.test.services.CarreraServiceTest;
+import com.edu.untref.gcu.test.services.PlanEstudioServiceTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CarreraServiceTest.class, PlanEstudioServiceTest.class })
